59e7eb2c4295f0d95df968e3db46d194376b7292
Floor Terra <floort@gmail.com>
2011-06-12 14:27:25 +0200

    Raw import of the cbp data

---
+++ import_raw_cbp.py
@@ -0,0 +1,24 @@
+import glob, json, os
+import couchdb
+
+def import_raw_cbp(directory, dbname="raw_cbp"):
+    """Import the raw json data from the cbpscraper.
+    """
+    couch = couchdb.Server()
+    # Create or get the database
+    try:
+        db = couch.create(dbname)
+    except couchdb.PreconditionFailed:
+        # Database already exists
+        db = couch[dbname]
+    for jsonfile in glob.glob(os.path.join(directory, "*.json")):
+        print jsonfile
+        data = json.load(open(jsonfile))
+        for company in data:
+            db[company["url"]] = company
+
+
+if __name__ == "__main__":
+    import_raw_cbp("/home/floort/devel/pim/data")
+
+
