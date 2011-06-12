22d563cd9862c1dd0dd86cf405ea33057a6a806e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-06-12 16:20:20 +0200

    Revisions and commits.

--- import_raw_cbp.py
+++ import_raw_cbp.py
@@ -1,26 +1,53 @@
-import glob, json, os, sys
+import glob, json, os, sys, time, logging
 import couchdb
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 def import_raw_cbp(directory, dbname="raw_cbp"):
     """Import the raw json data from the cbpscraper.
     """
-    couch = couchdb.Server()
+    couch = couchdb.Server(full_commit=False)
     # Create or get the database
     try:
         db = couch.create(dbname)
     except couchdb.PreconditionFailed:
         # Database already exists
         db = couch[dbname]
+
+
+    counter = 0
+    start_time = time.time()
     for jsonfile in glob.glob(os.path.join(directory, "*.json")):
-        print jsonfile
+        logging.debug('Reading %s', jsonfile)
         data = json.load(open(jsonfile))
         for company in data:
-            # Gebruik de url als key
-            if not company["url"] in db:
-                db[company["url"]] = company
+            doc_id = company['url']
+            doc = db.get(doc_id)
+
+            if doc:
+                logger.info('Found existing object for %s, updating.',
+                            doc_id)
+                doc.update(company)
             else:
-                print "Duplicate data in", jsonfile
-                print "skipping", company["url"]
+                doc = company
+
+            db[doc_id] = doc
+
+        counter += 1
+
+        if counter % 100 == 0:
+            logger.info('Wrote %d records to database in %f seconds',
+                        counter, time.time()-start_time)
+
+            if counter % 1000 == 0:
+                logger.info('Committing changes.')
+                db.commit()
+
+    logger.info('Wrote %d records to database in %f seconds',
+                counter, time.time()-start_time)
 
 
 if __name__ == "__main__":
