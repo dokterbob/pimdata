36a79e644197b73c276731aae8529e5195fc0510
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-06-12 16:36:26 +0200

    Optimization galore.

--- import_raw_kvk.py
+++ import_raw_kvk.py
@@ -10,16 +10,18 @@ def import_raw_kvk(csv_file, dbname="raw_kvk"):
     """Import the raw json data from Open KVK.
     """
     couch = couchdb.Server()
-    
+
     if dbname in couch: # Start fresh
         couch.delete(dbname)
     db = couch.create(dbname)
 
     reader = get_csv_reader(csv_file, 'utf-8')
-    
+
     counter = 0
     start_time = time.time()
     for row in reader:
+        records = []
+
         # Fieldnames from http://api.openkvk.nl/
         fields = {
             'kvk': row[0],
@@ -33,15 +35,17 @@ def import_raw_kvk(csv_file, dbname="raw_kvk"):
             'website': row[8]
         }
 
-        db.save(fields)
-        
+        records.append(fields)
         counter += 1
-        
-        if counter % 100 == 0:
-            logger.info('Wrote %d records to database in %f seconds',
-                        counter, time.time()-start_time)
 
-    logger.info('Wrote %d records to database in %f seconds', 
+        if counter % 100000 == 0:
+            db.update(records)
+
+            t = time.time()-start_time
+            logger.info('Wrote %d records to database in %f seconds (%f/s)',
+                        counter, t, counter/t)
+
+    logger.info('Wrote %d records to database in %f seconds',
                 counter, time.time()-start_time)
 
 
